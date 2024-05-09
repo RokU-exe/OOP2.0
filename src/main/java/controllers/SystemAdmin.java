f2d4f65745ee09975060e45609dabdb587c70940
thaobwi <129365706+thaobwi@users.noreply.github.com>
2024-05-09 21:22:38 +0700

    Update SystemAdmin.java

--- src/main/java/controllers/SystemAdmin.java
+++ src/main/java/controllers/SystemAdmin.java
@@ -4,8 +4,21 @@ import models.User;
 import models.UserRole;
 
 public class SystemAdmin extends User {
+    private static SystemAdmin instance; // singleton
+
     public SystemAdmin(String id, String fullName, String email, String password, UserRole role) {
         super(id, fullName, email, password, role);
     }
+
+    public SystemAdmin() {
+        super();
+    }
+
     // Additional methods specific to System Admins
+    public static SystemAdmin getInstance() {
+        if (instance == null) {
+            instance = new SystemAdmin();
+        }
+        return instance;
+    }
 }
