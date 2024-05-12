1156e1e56764c1df6ce32f69f2f9a85ddee14146
thaobwi <129365706+thaobwi@users.noreply.github.com>
2024-05-12 14:44:31 +0700

    Update Customer.java

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -2,54 +2,16 @@ package models;
 
 import java.util.List;
 
-public abstract class Customer {
-    private String id;
-    private String fullName;
+public abstract class Customer extends User{
     private InsuranceCard insuranceCard;
     private List<Claim> claims;
-    private String address;
-    private String phone;
-    private String email;
-    private String role;
 
-    public Customer(String id, String fullName, InsuranceCard insuranceCard, List<Claim> claims, String address, String phone, String email, String role) {
-        this.id = id;
-        this.fullName = fullName;
+    public Customer(String id, String fullName, String email, String password, UserRole role, InsuranceCard insuranceCard, List<Claim> claims) {
+        super(id, fullName, email, password, role);
         this.insuranceCard = insuranceCard;
         this.claims = claims;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-        this.role = role;
     }
 
-    // Constructors, getters, and setters
-    public Customer(String id, String fullName, InsuranceCard insuranceCard, List<Claim> claims,
-                    String address, String phone, String email) {
-        this.id = id;
-        this.fullName = fullName;
-        this.insuranceCard = insuranceCard;
-        this.claims = claims;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 
     public InsuranceCard getInsuranceCard() {
         return insuranceCard;
@@ -67,49 +29,11 @@ public abstract class Customer {
         this.claims = claims;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole(){
-        return this.role = role;
-    }
-
-    public String setRole(String role) {
-        return role;
-    }
-
     @Override
     public String toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", insuranceCard=" + insuranceCard +
+        return "Customer{" + super.toString()+
+                "insuranceCard=" + insuranceCard +
                 ", claims=" + claims +
-                ", address='" + address + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", role='" + role + '\'' +
                 '}';
     }
 }
