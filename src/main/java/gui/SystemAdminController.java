e3bc915cf1dbbd2bc93315a5c05fe636de1031b7
giabaodx <118871100+giabaodx@users.noreply.github.com>
2024-05-14 00:03:39 -0500

    Update SystemAdminController.java

--- src/main/java/gui/SystemAdminController.java
+++ src/main/java/gui/SystemAdminController.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import models.SystemAdmin;
+import controllers.SystemAdmin;
 import models.User;
 import models.UserRole;
 import utils.DBUtil;
